14b0a9b187935ad70d233e4da3b823d50608f27c
Ademola Olufemi <32356931+Ademola6980@users.noreply.github.com>
2021-12-02 15:17:36 +0100

    Add project files.

---
+++ Models/EmployeeVM.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Leave_Management.Models
+{
+    public class EmployeeVM
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public String Firstname { get; set; }
+        public String Lastname { get; set; }
+        public string TaxId { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public DateTime DateJoined { get; set; }
+        
+    }
+}
