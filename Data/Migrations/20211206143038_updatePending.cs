6f0028f1d0fddbde54e74618f4c79f7b541c3c1b
Ademola Olufemi <32356931+Ademola6980@users.noreply.github.com>
2021-12-10 12:33:33 +0100

    Setup user Auth, Default roles and Modified registration process

---
+++ Data/Migrations/20211206143038_updatePending.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Leave_Management.Data.Migrations
+{
+    public partial class updatePending : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DateCreated",
+                table: "AspNetUsers");
+
+            migrationBuilder.AddColumn<string>(
+                name: "TaxId",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "DetailsLeaveTypeVM",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DateCreated = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DetailsLeaveTypeVM", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DetailsLeaveTypeVM");
+
+            migrationBuilder.DropColumn(
+                name: "TaxId",
+                table: "AspNetUsers");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateCreated",
+                table: "AspNetUsers",
+                type: "datetime2",
+                nullable: true);
+        }
+    }
+}
